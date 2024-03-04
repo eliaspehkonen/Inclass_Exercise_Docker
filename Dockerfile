b77606ee5e907c5161462c5c94afbc3463f33dea
Elias Pehkonen <elias.pehkonen@metropolia.fi>
2024-03-04 15:26:57 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM maven:latest
+WORKDIR app
+COPY pom.xml /app/
+COPY . /app/
+RUN mvn package
+#CMD ["java", "-jar", "target/dockerdemo.jar"]
+ENTRYPOINT ["java", "-jar", "target/Inclass_exercise_docker.jar"]
---
+++ src/test/java/EliasDockerTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class EliasDockerTest {
+    EliasDocker eliasDocker = new EliasDocker();
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
